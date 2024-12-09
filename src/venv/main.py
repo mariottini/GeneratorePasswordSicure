fadf87bb2843f623f19bd56007036e9d6d1b2a67
BelalBvc <62206745+BelalBvc@users.noreply.github.com>
2024-12-09 14:24:31 +0100

    Update main.py

--- src/venv/main.py
+++ src/venv/main.py
@@ -11,30 +11,4 @@ pwGenerator = PasswordGenerator()
 # print(f"5 random lowercase chars:\n {random.choices(pwGenerator.lowercase, k=5)}") 
 
 
-while True:
-    try:
-     LunghezzPass = int(input("Inserire quanti caratteri deve essere lunga la password: "))
-     NumCorrispondeAllaLista = input("Quali set di caratteri vuoi che vengano utilizzati nellagenerazione della password?\n"
-      "[1] lowercase\n"
-      "[2] uppercase\n"
-      "[3] digits\n"
-      "[4] specialChars\n")
-     NumCorrispondeAllaLista = list(NumCorrispondeAllaLista)
-     i=0
-     NumProv = []
-     NuovoArray = []
-     while i < len(NumCorrispondeAllaLista):
-         numeroCost = int(NumCorrispondeAllaLista[i])
-         NumProv.append(numeroCost)
-         if(NumProv[i]>4 or NumProv[i]<0):
-             raise ValueError("Attenzione, inserire solamente cifre sotto il 5")
-         if(NumProv[i] not in NuovoArray):
-            NuovoArray.append(NumProv[i])
-         i = i + 1
-     print("questo è il nuovo array")
-     print(NuovoArray)
-    except (ValueError, Exception):
-       print("No attento, hai inserito un input non valido!, riprova")
-       
-    else: 
-          break
+
