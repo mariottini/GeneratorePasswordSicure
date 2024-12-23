440cb8b8e996d7161467cab69aa85358d98978c1
Matteo Mariottini <matteomariottini9@gmail.com>
2024-12-23 18:21:37 +0100

    simple_gui.py completed + small fixes in PasswordGenerator.py

--- src/PasswordGenerator.py
+++ src/PasswordGenerator.py
@@ -21,6 +21,7 @@ class PasswordGenerator:
         Costruisce la lista charsAvailable, contenente i tipi di carattere scelti dall'utente
     '''
     def buildCharsAvailable(self, choices):
+        self.charsAvailable = []
         for i in choices: # Scorre una lista contenente i tipi di carattere scelti dall'utente
             if i == 1: # Lettere minuscole
                 for j in self.lowercase:
@@ -43,6 +44,7 @@ class PasswordGenerator:
         la scelta viene ripetuta fino a raggiungere il valore di lunghezza fornito dall'utente
     '''
     def buildPassword(self, length, charsAvailable):
+        self.password = ""
         i = 0
         while i < length: # Ripete fino a raggiungere la lunghezza inserita dall'utente
             randomChoice = random.randint(0, len(charsAvailable)-1) # Genera un indice casuale tra 0 e la lunghezza della lista charsAvailable-1.
@@ -83,8 +85,8 @@ class PasswordGenerator:
         [Entropia → Misurazione in bit dell'imprevedibilità di una password]
     '''
     def pwStrength(self, password):
-        pwToList = list(password)  # Converte la password in una lista di caratteri
         entropy = 0  # Inizializza l'entropia a zero
+        pwToList = list(password)  # Converte la password in una lista di caratteri
         # Inizializzazione booleani per aggiornare la variante dei carateri solamente una volta per ogni set di caratteri
         hasLowercase = False
         hasUppercase = False
@@ -138,7 +140,34 @@ class PasswordGenerator:
         minutes = seconds/60
         hours = minutes/60
         days = hours/24
+        months = days/30
         years = days/365
+        centuries = years/100
+        millennia = years/1000
 
-        return f"Se un PC eseguisse un attacco brute force, per violare la password dovrebbe effettuare al massimo {tries:.0f} tentativi.\n" \
-                f"Se il PC esegue l'attacco ad una velcoità di 10^{int(math.log10(triesPerSec))} tentativi al secondo, ci impiegherà {years:.0f} anni / {days:.0f} giorni / {hours:.0f} ore / {minutes:.0f} minuti / {seconds:.2f} secondi."
+        if millennia > 1:
+            time = f"più di un millennio"
+        elif millennia == 1:
+            time = f"{millennia:.0f} millenni"
+        elif centuries >= 1:
+            time = f"{centuries:.0f} secoli"
+        elif years >= 1:
+            time = f"{years:.0f} anni" 
+        elif months >= 1:
+            time = f"{months:.0f} mesi"
+        elif days >= 1: 
+            time = f"{days:.0f} giorni" 
+        elif hours >= 1: 
+            time = f"{hours:.0f} ore" 
+        elif minutes >= 1: 
+            time = f"{minutes:.0f} minuti" 
+        else: 
+            time = f"{seconds:.5f} secondi"
+
+        if tries > 999999:
+            printTries = "anche più di un miliardo di"
+        else:
+            printTries = f"{tries:.0f}"
+
+        return f"Se un PC eseguisse un attacco brute force, per violare la password potrebbe effettuare {printTries} tentativi.\n" \
+                f"Se il PC eseguisse l'attacco ad una velcoità di 10^{int(math.log10(triesPerSec))} tentativi al secondo, ci impiegherebbe {time}"
